406d649774117d0d78acd7dff5693cf5321dcdfd
Santhiyahbn <147794226+Santhiyahbn@users.noreply.github.com>
2023-10-13 13:14:25 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,10 @@
+def linearsearchproduct(productlist,targetproduct) : 
+indices = []
+for index, product in enumerate(product list):
+if product == target product:
+return indices 
+#Example usage: 
+Product = ["shoes","boot","laafer","shoes","shoes","sandal","shoes"]
+target = "shoes"
+result = linearsearchproduct(product, target)
+print (result)

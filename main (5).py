876bc7d321b32f48a57213101a7f44bafbbb4895
Santhiyahbn <147794226+Santhiyahbn@users.noreply.github.com>
2023-10-13 13:14:09 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,18 @@
+# define the base class player 
+def player : 
+def play(self):
+print ("The player is playing cricket")
+
+Class Batsman(player):
+def play(self): 
+print ("The batsman is batting") 
+
+class Bowler(player): 
+  def play(self): 
+print("The bowler is bowing")
+
+batsman=Batsman() 
+bowler=Bowler() 
+
+batsman. play() 
+bowler. play ()
